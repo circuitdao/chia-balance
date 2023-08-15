5976ded8da15a2247a4905c080d6745d15f67d32
Sebastjan Trepca <trepca@gmail.com>
2023-08-15 11:38:27 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  import {
+    fns,
+    AppShell,
+    Navbar,
+    Header,
+    Title,
+    Divider,
+    Container,
+  } from "@svelteuidev/core";
+  import HeadContent from "./HeadContent.svelte";
+  import { Input, InputWrapper } from "@svelteuidev/core";
+
+  import { SvelteUIProvider } from "@svelteuidev/core";
+</script>
+
+<SvelteUIProvider>
+  <Container>
+    <HeadContent />
+
+    <slot>
+      <InputWrapper
+        id="pubkey"
+        label="Your master public key"
+        description="Enter your public key and we'll derive observable addresses and fetch the balances for you. It takes a few seconds."
+      >
+        <Input
+          name="master_pubkey"
+          value=""
+          placeholder="insert your master public key, derivations will be done in the browser"
+        />
+      </InputWrapper>
+    </slot>
+  </Container>
+</SvelteUIProvider>
---
+++ src/routes/HeadContent.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Title } from "@svelteuidev/core";
+</script>
+
+<p>
+  <Title order={1}>Chia address balance</Title>
+</p>
+
